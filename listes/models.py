ceeeb72a6a51b59510ad19dfaea65fcca66a3ae7
Matthieu <lourme78@gmail.com>
2021-02-02 00:36:33 +0100

    Add Professor, User and Admin entities

--- listes/models.py
+++ listes/models.py
@@ -1,28 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
 import datetime
 from django.utils import timezone
 
-class Question(models.Model):
-   question_text = models.CharField(max_length=200)
-   pub_date = models.DateTimeField('date published')
+class Utilisateur(models.Model):
+   mdp = models.CharField(max_length=100)
+   nom = models.CharField(max_length=100)
+   prenom = models.CharField(max_length=100)
+   email = models.EmailField(max_length=100)
+
+   class Meta:
+       abstract = True
+
+class Admnistrateur(Utilisateur):
+   poste = models.CharField(max_length=100)
+
 
    def __str__(self):
-       return self.question_text
+       return self.poste
 
-   def was_published_recently(self):
-       now = timezone.now()
-       return now - datetime.timedelta(days=1) <= self.pub_date <= now
+class Titre(models.TextChoices):
+    doctorant= 'Doctorant',
+    maitre_de_conferences = 'Maître de conférences',
+    prof_des_universites = 'Professeur des universités',
+    tuteur = 'Tuteur'
 
-   was_published_recently.admin_order_field = 'pub_date'
-   was_published_recently.boolean = True
-   was_published_recently.short_description = 'Published recently?'
+class Professeur(Utilisateur):
+   titre = models.CharField(max_length=100,choices=Titre.choices)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING,)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.choice_text
+   def __str__(self):
+       return self.titre
+
--- listes/urls.py
+++ listes/urls.py
@@ -2,11 +2,4 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-url(r'^$', views.index, name='index'),
-# ex: /listes/5/
-url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-# ex: /listes/5/results/
-url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-# ex: /listes/5/vote/
-url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
